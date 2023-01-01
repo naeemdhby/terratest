45bcab3e83fc4092733b79248236ca6ae392cc52
Naeim Beigi <20710318+naeemdhby@users.noreply.github.com>
2022-12-31 16:38:17 -0800

    Create types.go

---
+++ types/types.go
@@ -0,0 +1,24 @@
+package main
+
+type ResourceGroups struct {
+	Variables struct {
+		ResourceGroups struct {
+			Value map[string]struct {
+				Name         string `json:"name"`
+				Region       string `json:"region"`
+				Subscription string `json:"subscription"`
+			} `json:"value"`
+		} `json:"resource_groups"`
+	} `json:"variables"`
+}
+
+type VirtualMachines struct {
+	Variables struct {
+		VirtualMachines struct {
+			Value map[string]struct {
+				Name              string `json:"name"`
+				ResourceGroupName string `json:"resource_group_name"`
+			} `json:"value"`
+		} `json:"virtual_machines"`
+	} `json:"variables"`
+}
